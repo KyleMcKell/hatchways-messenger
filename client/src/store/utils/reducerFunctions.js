9169efd0c366f9e9d70c8db6b769e3cc43b160bd
Kyle McKell <46072760+KyleMcKell@users.noreply.github.com>
2022-01-27 17:30:26 -0700

    changed state mapping to be fully immutable by removing push in favor of spreading previous messages and convo

--- client/src/store/utils/reducerFunctions.js
+++ client/src/store/utils/reducerFunctions.js
@@ -1,80 +1,81 @@
 export const addMessageToStore = (state, payload) => {
-  const { message, sender } = payload;
-  // if sender isn't null, that means the message needs to be put in a brand new convo
-  if (sender !== null) {
-    const newConvo = {
-      id: message.conversationId,
-      otherUser: sender,
-      messages: [message],
-    };
-    newConvo.latestMessageText = message.text;
-    return [newConvo, ...state];
-  }
+	const { message, sender } = payload;
+	// if sender isn't null, that means the message needs to be put in a brand new convo
+	if (sender !== null) {
+		const newConvo = {
+			id: message.conversationId,
+			otherUser: sender,
+			messages: [message],
+		};
+		newConvo.latestMessageText = message.text;
+		return [newConvo, ...state];
+	}
 
-  return state.map((convo) => {
-    if (convo.id === message.conversationId) {
-      convo.messages.push(message);
-      convo.latestMessageText = message.text;
-      return convo;
-    } else {
-      return convo;
-    }
-  });
+	return state.map((convo) => {
+		if (convo.id === message.conversationId) {
+			const convoCopy = { ...convo };
+			convoCopy.messages = [message, ...convoCopy.messages];
+			convoCopy.latestMessageText = message.text;
+			return convoCopy;
+		} else {
+			return convo;
+		}
+	});
 };
 
 export const addOnlineUserToStore = (state, id) => {
-  return state.map((convo) => {
-    if (convo.otherUser.id === id) {
-      const convoCopy = { ...convo };
-      convoCopy.otherUser = { ...convoCopy.otherUser, online: true };
-      return convoCopy;
-    } else {
-      return convo;
-    }
-  });
+	return state.map((convo) => {
+		if (convo.otherUser.id === id) {
+			const convoCopy = { ...convo };
+			convoCopy.otherUser = { ...convoCopy.otherUser, online: true };
+			return convoCopy;
+		} else {
+			return convo;
+		}
+	});
 };
 
 export const removeOfflineUserFromStore = (state, id) => {
-  return state.map((convo) => {
-    if (convo.otherUser.id === id) {
-      const convoCopy = { ...convo };
-      convoCopy.otherUser = { ...convoCopy.otherUser, online: false };
-      return convoCopy;
-    } else {
-      return convo;
-    }
-  });
+	return state.map((convo) => {
+		if (convo.otherUser.id === id) {
+			const convoCopy = { ...convo };
+			convoCopy.otherUser = { ...convoCopy.otherUser, online: false };
+			return convoCopy;
+		} else {
+			return convo;
+		}
+	});
 };
 
 export const addSearchedUsersToStore = (state, users) => {
-  const currentUsers = {};
+	const currentUsers = {};
 
-  // make table of current users so we can lookup faster
-  state.forEach((convo) => {
-    currentUsers[convo.otherUser.id] = true;
-  });
+	// make table of current users so we can lookup faster
+	state.forEach((convo) => {
+		currentUsers[convo.otherUser.id] = true;
+	});
 
-  const newState = [...state];
-  users.forEach((user) => {
-    // only create a fake convo if we don't already have a convo with this user
-    if (!currentUsers[user.id]) {
-      let fakeConvo = { otherUser: user, messages: [] };
-      newState.push(fakeConvo);
-    }
-  });
+	const newState = [...state];
+	users.forEach((user) => {
+		// only create a fake convo if we don't already have a convo with this user
+		if (!currentUsers[user.id]) {
+			let fakeConvo = { otherUser: user, messages: [] };
+			newState.push(fakeConvo);
+		}
+	});
 
-  return newState;
+	return newState;
 };
 
 export const addNewConvoToStore = (state, recipientId, message) => {
-  return state.map((convo) => {
-    if (convo.otherUser.id === recipientId) {
-      convo.id = message.conversationId;
-      convo.messages.push(message);
-      convo.latestMessageText = message.text;
-      return convo;
-    } else {
-      return convo;
-    }
-  });
+	return state.map((convo) => {
+		if (convo.otherUser.id === recipientId) {
+			convo.id = message.conversationId;
+			convo.messages.push(message);
+			convo.latestMessageText = message.text;
+			return convo;
+		} else {
+			return convo;
+		}
+	});
 };
