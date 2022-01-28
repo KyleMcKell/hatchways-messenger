6350d44711d63290d2d6527c344654ea83052b9f
Kyle McKell <46072760+KyleMcKell@users.noreply.github.com>
2022-01-27 23:50:27 -0700

    removed modified db.js file from pull request as it isn't pertinent to current implementation but rather my dev environemtn

--- server/db/db.js
+++ server/db/db.js
@@ -1,12 +1,7 @@
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
-const db = new Sequelize(
-	process.env.DATABASE_URL ||
-		'postgres://postgres:postgres@localhost:5432/messenger',
-	{
-		dialect: 'postgres',
-		logging: false,
-	}
-);
+const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
+  logging: false
+});
 
 module.exports = db;
