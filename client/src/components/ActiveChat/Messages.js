e3d4f4f149516f2fb634ec2f04d3dbb7d82a3a61
Kyle McKell <46072760+KyleMcKell@users.noreply.github.com>
2022-01-28 19:27:13 -0700

    removed sorting on component level and removing style lint that happened due to my vscode prettier environment

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -8,18 +8,13 @@ const Messages = (props) => {
 
   return (
     <Box>
-      {[...messages].reverse().map((message) => {
+      {messages.map((message) => {
         const time = moment(message.createdAt).format("h:mm");
 
         return message.senderId === userId ? (
           <SenderBubble key={message.id} text={message.text} time={time} />
         ) : (
-          <OtherUserBubble
-            key={message.id}
-            text={message.text}
-            time={time}
-            otherUser={otherUser}
-          />
+          <OtherUserBubble key={message.id} text={message.text} time={time} otherUser={otherUser} />
         );
       })}
     </Box>
