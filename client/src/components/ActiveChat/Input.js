6314fdcd9cda3bd36cb6e8405184df41edb54a29
Kyle McKell <46072760+KyleMcKell@users.noreply.github.com>
2022-02-01 16:00:00 -0700

    made a file upload input and a function that displays images as a preview before images are sent

--- client/src/components/ActiveChat/Input.js
+++ client/src/components/ActiveChat/Input.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { FormControl, FilledInput } from "@material-ui/core";
+import { FormControl, FilledInput, Grid } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import { connect } from "react-redux";
 import { postMessage } from "../../store/utils/thunkCreators";
@@ -13,7 +13,23 @@ const useStyles = makeStyles(() => ({
     height: 70,
     backgroundColor: "#F4F6FA",
     borderRadius: 8,
+    marginTop: 20,
     marginBottom: 20
+  },
+  previewGrid: {
+    // width: "100%"
+    display: "grid",
+    gridTemplateColumns: "repeat(auto-fill, minmax(100px, 1fr))",
+    gap: "10px",
+    background: "#F4F6FA",
+    padding: 10,
+    borderRadius: 8
+  },
+  previewImg: {
+    borderRadius: 8,
+    width: 100,
+    height: 100,
+    objectFit: "cover"
   }
 }));
 
@@ -21,19 +37,35 @@ const Input = (props) => {
   const classes = useStyles();
   const [text, setText] = useState("");
   const { postMessage, otherUser, conversationId, user } = props;
+  const [imageSrcs, setImageSrcs] = useState([]);
+  // const [attachments, setAttachments] = useState([]);
 
-  const handleChange = (event) => {
+  const handleTextChange = (event) => {
     setText(event.target.value);
+  };
+
+  const handleFileChange = (fileChangeEvent) => {
+    const files = fileChangeEvent.target.files;
+    for (let i = 0; i < files.length; i++) {
+      const file = files[i];
+      const reader = new FileReader();
+      reader.onload = function (e) {
+        setImageSrcs((previousSrcs) => [...previousSrcs, e.target.result]);
+      };
+      reader.readAsDataURL(file);
+    }
   };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
+
     // add sender user info if posting to a brand new convo, so that the other user will have access to username, profile pic, etc.
     const reqBody = {
       text: event.target.text.value,
       recipientId: otherUser.id,
       conversationId,
       sender: conversationId ? null : user
+      // attachments
     };
     await postMessage(reqBody);
     setText("");
@@ -41,6 +73,19 @@ const Input = (props) => {
 
   return (
     <form className={classes.root} onSubmit={handleSubmit}>
+      {imageSrcs.length > 0 && (
+        <Grid className={classes.previewGrid}>
+          {imageSrcs.map((src, index) => (
+            <img
+              key={index}
+              src={src}
+              alt="preview"
+              className={classes.previewImg}
+            />
+          ))}
+        </Grid>
+      )}
+
       <FormControl fullWidth hiddenLabel>
         <FilledInput
           classes={{ root: classes.input }}
@@ -48,7 +93,17 @@ const Input = (props) => {
           placeholder="Type something..."
           value={text}
           name="text"
-          onChange={handleChange}
+          onChange={handleTextChange}
+        />
+      </FormControl>
+      <FormControl>
+        <input
+          accept="image/*"
+          type="file"
+          name="file"
+          id="file"
+          onChange={handleFileChange}
+          multiple
         />
       </FormControl>
     </form>
@@ -59,7 +114,7 @@ const mapDispatchToProps = (dispatch) => {
   return {
     postMessage: (message) => {
       dispatch(postMessage(message));
-    },
+    }
   };
 };
 
