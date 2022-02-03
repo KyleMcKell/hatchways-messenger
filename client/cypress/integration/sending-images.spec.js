169915212aec03565d97951f63aac66c6b1e9980
Kyle McKell <46072760+KyleMcKell@users.noreply.github.com>
2022-02-02 19:15:34 -0700

    added more tests

--- client/cypress/integration/sending-images.spec.js
+++ client/cypress/integration/sending-images.spec.js
@@ -5,6 +5,7 @@ const thomas = {
   password: "123456"
 };
 const previewImgsClass = "*[class^=makeStyles-previewImg-]";
+const sentImgsClass = "*[class^=makeStyles-attachment-]";
 const fixtureImg = "mountain.jpg";
 
 describe("Feature: Sending Images", () => {
@@ -27,8 +28,32 @@ describe("Feature: Sending Images", () => {
     cy.get(previewImgsClass).should("have.length", 4);
   });
 
-  it("sends image attachments", () => {
+  it("sends one image attachment", () => {
     cy.reload();
     cy.login(thomas.username, thomas.password);
+
+    cy.get("input[name=search]").type("santiago");
+    cy.contains("santiago").click();
+
+    // create message with one attachment
+    cy.get("input[name=file]").attachFile(fixtureImg).type("{enter}");
+
+    // add text to make it so we can send message with cypress
+    cy.get(sentImgsClass, { timeout: 10000 }).should("have.length", 1);
+  });
+
+  it("sends multiple image attachments", () => {
+    cy.reload();
+    cy.login(thomas.username, thomas.password);
+
+    cy.get("input[name=search]").type("hualing");
+    cy.contains("hualing").click();
+
+    // create message with multiple attachments
+    cy.get("input[name=file]")
+      .attachFile([fixtureImg, fixtureImg])
+      .type("{enter}");
+
+    cy.get(sentImgsClass, { timeout: 10000 }).should("have.length", 2);
   });
 });
