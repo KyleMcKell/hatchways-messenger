c40f59c0cfa79d020d573e7da3b0f04e8f9db5bc
Kyle McKell <46072760+KyleMcKell@users.noreply.github.com>
2022-02-02 18:23:28 -0700

    putting a few variables into tests and adding cypress script

--- client/cypress/integration/sending-images.spec.js
+++ client/cypress/integration/sending-images.spec.js
@@ -1,22 +1,34 @@
 /// <reference types="cypress" />
 
+const thomas = {
+  username: "thomas",
+  password: "123456"
+};
+const previewImgsClass = "*[class^=makeStyles-previewImg-]";
+const fixtureImg = "mountain.jpg";
+
 describe("Feature: Sending Images", () => {
   it("shows image preview on file upload", () => {
-    cy.login("thomas", "123456");
+    cy.login(thomas.username, thomas.password);
 
     cy.get("input[name=search]").type("santiago");
     cy.contains("santiago").click();
 
     // check a singular attachment
-    cy.get("input[name=file]").attachFile("mountain.jpg");
-    cy.get("*[class^=makeStyles-previewImg-]").should("have.length", 1);
+    cy.get("input[name=file]").attachFile(fixtureImg);
+    cy.get(previewImgsClass).should("have.length", 1);
 
     // check multiple attachments uploaded one after another
-    cy.get("input[name=file]").attachFile("mountain.jpg");
-    cy.get("*[class^=makeStyles-previewImg-]").should("have.length", 2);
+    cy.get("input[name=file]").attachFile(fixtureImg);
+    cy.get(previewImgsClass).should("have.length", 2);
 
     // check multiple attachments uploaded at the same time
-    cy.get("input[name=file]").attachFile(["mountain.jpg", "mountain.jpg"]);
-    cy.get("*[class^=makeStyles-previewImg-]").should("have.length", 4);
+    cy.get("input[name=file]").attachFile([fixtureImg, fixtureImg]);
+    cy.get(previewImgsClass).should("have.length", 4);
+  });
+
+  it("sends image attachments", () => {
+    cy.reload();
+    cy.login(thomas.username, thomas.password);
   });
 });
